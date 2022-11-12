a05f6d283e58ccf6d0f24f3b64b594addbc393a7
fillikos <50418836+fillikos@users.noreply.github.com>
2022-11-12 13:02:39 +0100

    Update Main.java

--- src/de/fillikos/rf2/service/webui/Main.java
+++ src/de/fillikos/rf2/service/webui/Main.java
@@ -5,10 +5,7 @@ import de.fillikos.rf2.service.webui.httpss.model.Connection;
 public class Main {
 
     public static void main(String[] args) {
-        // Liveserver
-        Connection server = new Connection("http://89.163.146.67:", "5735");
-
-
+        
     }
 
 
